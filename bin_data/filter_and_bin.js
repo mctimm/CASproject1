7a0d440e7742dbd5638dcb75f5fc6a9811b44973
Sean Timm <seantimm3@aol.com>
2022-02-18 21:22:28 -0700

    add: new data for IT and FR!

--- bin_data/filter_and_bin.js
+++ bin_data/filter_and_bin.js
@@ -9,8 +9,6 @@ let country_code2 = "IT";
 let rawdata = fs.readFileSync("countries.json");
 let countries = JSON.parse(rawdata).records;
 
-fs.writeFileSync(`${country_code}_for_jidit.txt`, jiditString);
-
 const getCountryJson = (country_code, records) => {
   let country_data = records.filter(
     (x) => x.geoId === country_code && x.cases >= 0
@@ -28,11 +26,48 @@ const getCountryJson = (country_code, records) => {
   // console.dir(binned_data);
   let data = JSON.stringify(binned_data);
   fs.writeFileSync(`${country_code}_binned_data.json`, data);
+  return binned_data;
 };
 
 // TODO problem to answer: how do we handle date mismatch?
-country1 = getCountryJson(country_code1, countries);
-country2 = getCountryJson(country_code2, countries);
+let country1 = getCountryJson(country_code1, countries);
+let country2 = getCountryJson(country_code2, countries);
 
-let jiditString = binned_data.reduce((x, y) => x + y.binned + "\n", "");
+let dates1 = country1.map((x) => x.dateRep);
+let dates2 = country2.map((x) => x.dateRep);
+let not_included_dates1 = [];
+let not_included_dates2 = [];
+let sum = 0;
+dates1.forEach((element) => {
+  if (dates2.includes(element)) {
+    sum += 1;
+  } else {
+    not_included_dates1.push(element);
+  }
+});
+dates2.forEach((element) => {
+  if (dates1.includes(element)) {
+    sum += 1;
+  } else {
+    not_included_dates2.push(element);
+  }
+});
+
+country1 = country1.filter((x) => !not_included_dates1.includes(x.dateRep));
+country2 = country2.filter((x) => !not_included_dates2.includes(x.dateRep));
+// console.log(country2.length);
+// console.log(country1.length);
+
+let jiditString = "";
+for (let index = country1.length - 1; index >= 0; index--) {
+  jiditString += country1[index].binned + " " + country2[index].binned + "\n";
+}
+console.log(country1[0]);
+console.log(country2[0]);
+// let jiditString = binned_data.reduce((x, y) => x + y.binned + "\n", "");
 console.log(jiditString);
+fs.writeFileSync(
+  `${country_code1} - ${country_code2} for_jidit.txt`,
+  jiditString
+);
+// console.log(`${sum} + ${not_included_dates.length}/${dates1.length} `);
