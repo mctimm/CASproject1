b12286255e9cafca977e65c5c768e94582aade48
Sean Timm <seantimm3@aol.com>
2022-02-20 12:17:29 -0700

    add: another visual.

--- bin_data/compare_countries.py
+++ bin_data/compare_countries.py
@@ -14,7 +14,7 @@ with open("matching_data/OverallMatchingData.json") as f:
         xs = list(map(int, xs))
         ys = obj['c2_data'].split(",")
         ys = list(map(int, ys))
-        entropy = pyinform.transfer_entropy(xs, ys, k=2)
+        entropy = pyinform.transfer_entropy(xs, ys, k=3)
         overall.append({"c1" : obj['c1_code'], "c2" : obj['c2_code'], "Transfer Entropy" : entropy, 'c1_temp' : temps[obj['c1_code']], 'c2_temp' : temps[obj['c2_code']], 'Combined Temperature' :  temps[obj['c1_code']] +  temps[obj['c2_code']] })
         # print("C1: {0}, C2: {1}".format(obj['c1_code'], obj['c2_code']))
         # print(str(entropy))
