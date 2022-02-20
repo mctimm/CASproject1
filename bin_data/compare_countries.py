f464270a3236ed8e1c39b22a4db94bdc72ec8cf8
Sean Timm <seantimm3@aol.com>
2022-02-20 10:05:47 -0700

    add: new visualizations and files.

--- bin_data/compare_countries.py
+++ bin_data/compare_countries.py
@@ -1,5 +1,6 @@
 import pyinform
 import json
+from operator import itemgetter
 overall = []
 temps = {}
 with open("average_temperature_for_our_countries.json") as fp:
@@ -14,11 +15,17 @@ with open("matching_data/OverallMatchingData.json") as f:
         ys = obj['c2_data'].split(",")
         ys = list(map(int, ys))
         entropy = pyinform.transfer_entropy(xs, ys, k=2)
-        overall.append({"c1" : obj['c1_code'], "c2" : obj['c2_code'], "transfer_entropy" : entropy, 'c1_temp' : temps[obj['c1_code']], 'c2_temp' : temps[obj['c2_code']]})
+        overall.append({"c1" : obj['c1_code'], "c2" : obj['c2_code'], "Transfer Entropy" : entropy, 'c1_temp' : temps[obj['c1_code']], 'c2_temp' : temps[obj['c2_code']], 'Combined Temperature' :  temps[obj['c1_code']] +  temps[obj['c2_code']] })
         # print("C1: {0}, C2: {1}".format(obj['c1_code'], obj['c2_code']))
         # print(str(entropy))
+    with open("average_temperature_for_our_countries.json") as fp:
+        temp_data = json.load(fp)
+        for x in temp_data:
+            overall.append({"c1" : x['country'], "c2" : x['country'], "Transfer Entropy" : None, 'c1_temp' : x['temp'], 'c2_temp' : x['temp']})
 # print(overall)
-f = open("transfer_entropy & temp!.json", "w")
+overall = sorted(overall, key=itemgetter('c1_temp', 'c2_temp'))
+overall.reverse()
+f = open("transfer_entropy & temp - sorted! with combined temperature.json", "w")
 printer = json.dump(overall, f)
 # f.write(printer)
 f.close()
